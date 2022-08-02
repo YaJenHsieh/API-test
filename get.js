95c89307ec41e868d851220deeba59c9478c4c09
YaJenHsieh <109647203+YaJenHsieh@users.noreply.github.com>
2022-08-02 21:02:51 +0800

    API test

--- get.js
+++ get.js
@@ -9,28 +9,45 @@ function getData(){
 		let str = "";
 		for(let i = 0; i < data.length;i++){
 			const news = data[i];
-
+			const newsItem = 
+				{
+					"title":news.標題,
+					"content":news.內容,
+					"connectionUrl":news.連結網址,
+					"dateOfIssue":news.發布日期,
+					"lastUpdateDate":news.修改日期,
+				}
+			//console.log(newsItem);
+			
 			let addStr="";
 				for(let j = 0 ; j < news.附加檔案.length;j++){
 					const dataNews = news.附加檔案[j];
-					console.log(dataNews);
+					const dataNewsItem = 
+						{
+							"descriptionFile":dataNews.檔案說明,
+							"fileName":dataNews.檔案名稱,
+							"connectionUrl":dataNews.連結位置
+						}
+					//console.log(dataNewsItem);
+
 					let addContent =`
 					<ul>
-					<li>${dataNews.檔案說明}</li>
-					<li>${dataNews.檔案名稱}</li>
-					<li>${dataNews.連結位置}</li>
+						<li>${dataNewsItem.descriptionFile}</li>
+						<li>${dataNewsItem.fileName}</li>
+						<li>${dataNewsItem.connectionUrl}</li>
 					</ul>`
 					addStr += addContent;
 				}
+
 			let content =`
 			<tr>
-				<td>${news.標題}</td>
-				<td>${news.內容}</td>
-				<td>${news.連結網址}</td>
+				<td>${newsItem.title}</td>
+				<td>${newsItem.content}</td>
+				<td>${newsItem.connectionUrl}</td>
 				<td>`+addStr+`</td>
-				<td>${news.發布日期}</td>
-				<td>${news.修改日期}</td>
-				</tr>`;
+				<td>${newsItem.dateOfIssue}</td>
+				<td>${newsItem.lastUpdateDate}</td>
+			</tr>`;
 			str+=content;
 			list.innerHTML=str;
 		}
